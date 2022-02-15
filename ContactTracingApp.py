3f7dfa797d888b8b1ec74a025a43cf5781963516
MariaBernadeteBaroto <bernadetebaroto@gmail.com>
2022-02-15 17:52:33 +0800

    writing a text file

--- ContactTracingApp.py
+++ ContactTracingApp.py
@@ -13,13 +13,19 @@ while True:
     data, bbox, _ = detector.detectAndDecode(img)
     # check if there is a QRCode in the image
     if data:
-        a = data
+        Info = data
         break
     cv2.imshow("QRCODEscanner", img)
     if cv2.waitKey(1) == ord("q"):
         break
 
 
-b = webbrowser.open(str(a))
+ContactTracingData = webbrowser.open(str(Info))
 cap.release()
 cv2.destroyAllWindows()
+
+lines = ['ContactTracingData.txt']
+with open('ContactTracingData.txt', 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
