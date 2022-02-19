004bb456ea10f74f847f068dd6271058469b13a8
MariaBernadeteBaroto <bernadetebaroto@gmail.com>
2022-02-19 17:21:37 +0800

    ContactTracingApp.py

--- ContactTracingApp.py
+++ ContactTracingApp.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
-import webbrowser 
+import webbrowser
 import datetime
 
 cap = cv2.VideoCapture(0)
@@ -13,21 +13,21 @@ print(now.strftime("%m-%d-%y %H:%M:%S"))
 
 while True:
     _, img = cap.read()
-    
-    decodedObjects = pyzbar.decode(img)
-    
 
+    data = pyzbar.decode(img)
+    #print(data)
     with open('qrcodeData.txt', 'w') as output:
+        output.writelines(str(data))
         output.write('Date and Time:')
         output.write(now.strftime("%m-%d-%y %H:%M:%S"))
-
-    
-    
     
     cv2.imshow("QRCODEscanner", img)
     key = cv2.waitKey(1)
-    if key == 30:
+    if key == ord('q'):
         break
+
+
+
 
 
 
